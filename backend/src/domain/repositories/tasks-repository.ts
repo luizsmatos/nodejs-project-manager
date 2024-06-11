6f7c54dce9d21529536f2fc52d976678a5c27287
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 12:32:11 -0300

    feat(tasks-repository.ts): add findById method to TasksRepository interface to retrieve a task by its ID

--- backend/src/domain/repositories/tasks-repository.ts
+++ backend/src/domain/repositories/tasks-repository.ts
@@ -2,6 +2,7 @@ import { PaginationParams } from '@/core/types/pagination-params'
 import { Task } from '../entities/task'
 
 export interface TasksRepository {
+  findById(taskId: string): Promise<Task | null>
   findManyByProjectId(
     projectId: string,
     pagination: PaginationParams,

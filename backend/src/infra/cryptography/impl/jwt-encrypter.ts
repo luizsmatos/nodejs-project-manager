28de4afafbc937954e63c107deef5da9bc8c934b
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 18:37:43 -0300

    feat(jwt-encrypter.ts): add JwtEncrypter class to implement Encrypter interface for JWT encryption functionality

---
+++ backend/src/infra/cryptography/impl/jwt-encrypter.ts
@@ -0,0 +1,11 @@
+import jwt from 'jsonwebtoken'
+import { env } from '@/infra/config/env'
+import { Encrypter } from '../encrypter'
+
+export class JwtEncrypter implements Encrypter {
+  async encrypt(payload: Record<string, unknown>): Promise<string> {
+    return jwt.sign(payload, env.JWT_SECRET, {
+      expiresIn: '1d',
+    })
+  }
+}

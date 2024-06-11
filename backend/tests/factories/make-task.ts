51736eca2deba1da678532e423b08ac9fc5af9c9
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 12:14:15 -0300

    feat(tests): add factory function makeTask to generate mock Task objects for testing purposes

---
+++ backend/tests/factories/make-task.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+
+import { Task, TaskStatus } from '@/domain/entities/task'
+
+export function makeTask(override: Partial<Task> = {}): Task {
+  return {
+    id: faker.string.uuid(),
+    projectId: faker.string.uuid(),
+    title: faker.person.fullName(),
+    description: faker.lorem.paragraph(),
+    status: TaskStatus.PENDING,
+    completedBy: faker.string.uuid(),
+    completedAt: null,
+    createdAt: faker.date.past(),
+    updatedAt: null,
+    ...override,
+  }
+}

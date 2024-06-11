d734d77297a9503cac5919427e4ffe82d102883b
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 12:46:46 -0300

    feat(edit-task-usecase.ts): add EditTaskUseCase class to handle editing tasks

---
+++ backend/src/domain/use-cases/edit-task-usecase.ts
@@ -0,0 +1,46 @@
+import { Task, TaskStatus } from '../entities/task'
+import { TasksRepository } from '../repositories/tasks-repository'
+import { TaskNotFoundError } from './errors/task-not-found-error'
+
+interface EditTaskUseCaseRequest {
+  taskId: string
+  title: string
+  description: string
+  status: TaskStatus
+  completedBy?: string
+}
+
+interface EditTaskUseCaseResponse {
+  task: Task
+}
+
+export class EditTaskUseCase {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  async execute({
+    taskId,
+    title,
+    description,
+    status,
+    completedBy,
+  }: EditTaskUseCaseRequest): Promise<EditTaskUseCaseResponse> {
+    const task = await this.tasksRepository.findById(taskId)
+
+    if (!task) {
+      throw new TaskNotFoundError()
+    }
+
+    task.title = title
+    task.description = description
+    task.status = status
+    task.completedBy = completedBy ?? null
+    task.completedAt = completedBy ? new Date() : null
+    task.updatedAt = new Date()
+
+    await this.tasksRepository.save(task)
+
+    return {
+      task,
+    }
+  }
+}

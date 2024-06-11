0792b5d3667ad38b8b1b72a67c3c94ba0490ac4b
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 12:15:25 -0300

    feat(list-project-tasks-usecase.spec.ts): add unit tests for ListProjectTasksUseCase to ensure correct listing of project tasks and pagination functionality

---
+++ backend/src/domain/use-cases/list-project-tasks-usecase.spec.ts
@@ -0,0 +1,47 @@
+import { InMemoryTasksRepository } from '#/repositories/in-memory-tasks-repository'
+import { makeProject } from '#/factories/make-project'
+import { makeTask } from '#/factories/make-task'
+
+import { ListProjectTasksUseCase } from './list-project-tasks-usecase'
+
+let sut: ListProjectTasksUseCase
+let inMemoryTasksRepository: InMemoryTasksRepository
+
+describe('List Project Tasks UseCase', () => {
+  beforeEach(() => {
+    inMemoryTasksRepository = new InMemoryTasksRepository()
+    sut = new ListProjectTasksUseCase(inMemoryTasksRepository)
+  })
+
+  it('should be able to list a project tasks', async () => {
+    const project01 = makeProject()
+    const project02 = makeProject()
+
+    inMemoryTasksRepository.items.push(makeTask({ projectId: project01.id }))
+    inMemoryTasksRepository.items.push(makeTask({ projectId: project01.id }))
+    inMemoryTasksRepository.items.push(makeTask({ projectId: project02.id }))
+
+    const result = await sut.execute({
+      projectId: project01.id,
+      page: 1,
+    })
+
+    expect(result.tasks).toHaveLength(2)
+    expect(inMemoryTasksRepository.items).toHaveLength(3)
+  })
+
+  it('should be able to list paginated tasks', async () => {
+    const project = makeProject()
+
+    for (let i = 1; i <= 22; i++) {
+      inMemoryTasksRepository.items.push(makeTask({ projectId: project.id }))
+    }
+
+    const result = await sut.execute({
+      projectId: project.id,
+      page: 2,
+    })
+
+    expect(result.tasks).toHaveLength(2)
+  })
+})

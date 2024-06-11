3f0fc4b756812307bc6ac9b543aeedbccdcfe60a
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 18:45:02 -0300

    feat(prisma-projects-repository.ts): implement PrismaProjectsRepository class to handle CRUD operations for projects using Prisma ORM

---
+++ backend/src/infra/database/prisma/repositories/prisma-projects-repository.ts
@@ -0,0 +1,63 @@
+import { PaginationParams } from '@/core/types/pagination-params'
+import { Project } from '@/domain/entities/project'
+import { ProjectsRepository } from '@/domain/repositories/projects-repository'
+import { prisma } from '../prisma'
+import { PrismaProjectMapper } from '../mappers/prisma-project-mapper'
+
+export class PrismaProjectsRepository implements ProjectsRepository {
+  async findById(projectId: string): Promise<Project | null> {
+    const project = await prisma.project.findUnique({
+      where: {
+        id: projectId,
+      },
+    })
+
+    if (!project) {
+      return null
+    }
+
+    return PrismaProjectMapper.toDomain(project)
+  }
+
+  async findManyByUserId(
+    userId: string,
+    { page }: PaginationParams,
+  ): Promise<Project[]> {
+    const projects = await prisma.project.findMany({
+      where: {
+        userId,
+      },
+      skip: (page - 1) * 20,
+      take: page * 20,
+    })
+
+    return projects.map((project) => PrismaProjectMapper.toDomain(project))
+  }
+
+  async delete(project: Project): Promise<void> {
+    await prisma.project.delete({
+      where: {
+        id: project.id,
+      },
+    })
+  }
+
+  async save(project: Project): Promise<void> {
+    const data = PrismaProjectMapper.toPrisma(project)
+
+    await prisma.project.update({
+      where: {
+        id: project.id,
+      },
+      data,
+    })
+  }
+
+  async create(project: Project): Promise<void> {
+    const data = PrismaProjectMapper.toPrisma(project)
+
+    await prisma.project.create({
+      data,
+    })
+  }
+}

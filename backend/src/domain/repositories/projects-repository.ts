b24facad4283c040144a36d934eb04cfdfe8722e
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 11:15:13 -0300

    feat(projects-repository.ts): add findById method to ProjectsRepository interface to retrieve a project by its ID

--- backend/src/domain/repositories/projects-repository.ts
+++ backend/src/domain/repositories/projects-repository.ts
@@ -3,6 +3,7 @@ import { PaginationParams } from '@/core/types/pagination-params'
 import { Project } from '../entities/project'
 
 export interface ProjectsRepository {
+  findById(projectId: string): Promise<Project | null>
   findManyByUserId(
     userId: string,
     pagination: PaginationParams,

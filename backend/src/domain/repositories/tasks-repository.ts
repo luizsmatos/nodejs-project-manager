0f6d12d73db2e57c570fdadfcd67b9fdfba13011
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 12:13:46 -0300

    feat(tasks-repository): create TasksRepository interface to define methods for retrieving tasks by project ID with pagination support

---
+++ backend/src/domain/repositories/tasks-repository.ts
@@ -0,0 +1,9 @@
+import { PaginationParams } from '@/core/types/pagination-params'
+import { Task } from '../entities/task'
+
+export interface TasksRepository {
+  findManyByProjectId(
+    projectId: string,
+    pagination: PaginationParams,
+  ): Promise<Task[]>
+}

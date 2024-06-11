2c36104cdd5669b6fba3cde6ec2de0c3ef8ee5fb
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 18:56:41 -0300

    feat(env.ts): add DATABASE_URL validation to ensure it is a valid URL string

--- backend/src/infra/config/env.ts
+++ backend/src/infra/config/env.ts
@@ -7,6 +7,7 @@ const envSchema = z.object({
     .default('development'),
   PORT: z.coerce.number().default(3333),
   JWT_SECRET: z.string(),
+  DATABASE_URL: z.string().url(),
 })
 
 const _env = envSchema.safeParse(process.env)

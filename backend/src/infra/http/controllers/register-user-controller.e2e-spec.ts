9ed6f9e81cee1d742c7f010403c0c446b5fdfdc3
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 21:33:48 -0300

    feat(register-user-controller.e2e-spec.ts): add end-to-end tests for Register User Controller

---
+++ backend/src/infra/http/controllers/register-user-controller.e2e-spec.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+import { app } from '../app'
+
+describe('Register User Controller (e2e)', () => {
+  it('should return 201 on success', async () => {
+    const response = await request(app).post('/auth/register').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+    expect(response.body).toEqual({
+      user: expect.objectContaining({
+        id: expect.any(String),
+      }),
+    })
+  })
+
+  it('should return 409 if email is already in use', async () => {
+    const response = await request(app).post('/auth/register').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(409)
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        message: 'User "johndoe@example.com" already exists',
+      }),
+    )
+  })
+})

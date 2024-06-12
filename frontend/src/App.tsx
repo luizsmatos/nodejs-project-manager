ed1158241d59f50346ee18b26fda1b642f6e8e26
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-12 14:07:45 -0300

    feat(App.tsx): add Toaster component with richColors prop to enhance user experience

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | project.manager" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )

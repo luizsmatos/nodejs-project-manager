76c43a3709d69508efe93fc503f9f574ae9b492e
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 22:45:37 -0300

    feat(delete-project-controller.e2e-spec.ts): add e2e test for Delete Project Controller

---
+++ backend/src/infra/http/controllers/delete-project-controller.e2e-spec.ts
@@ -0,0 +1,27 @@
+import request from 'supertest'
+import { faker } from '@faker-js/faker'
+import { createAndAuthenticateUser } from '#/utils/create-and-authenticate-user'
+import { app } from '../app'
+
+describe('Delete Project Controller (e2e)', () => {
+  it('should return 204 on success', async () => {
+    const { accessToken } = await createAndAuthenticateUser(app)
+
+    const createProject = await request(app)
+      .post('/projects')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: faker.lorem.word(5),
+        description: faker.lorem.paragraph(),
+      })
+
+    const projectId = createProject.body.project.id
+
+    const response = await request(app)
+      .delete(`/projects/${projectId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toEqual(204)
+  })
+})

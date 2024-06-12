fbc8de28f9c36a6ed7c9f3bc83c1a832afdb372e
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-12 18:33:54 -0300

    refactor(sidebar.tsx): refactor sidebar component to use Projects component instead of individual project components for improved code organization and maintainability

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,28 +1,9 @@
-import { PlusIcon } from 'lucide-react'
-
-import { CreateProject } from './project/create-project'
-import { ListProjects } from './project/list-projects'
-import { ProjectFilters } from './project/project-filters'
-import { Button } from './ui/button'
-import { Dialog, DialogTrigger } from './ui/dialog'
+import { Projects } from './projects/projects'
 
 export function SideBar() {
   return (
-    <div className="flex flex-col gap-6 border-r border-gray-200 p-6 dark:border-gray-700">
-      <ProjectFilters />
-
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button variant="outline" className="w-full">
-            <PlusIcon className="mr-2 h-4 w-4" />
-            Novo Projeto
-          </Button>
-        </DialogTrigger>
-
-        <CreateProject />
-      </Dialog>
-
-      <ListProjects />
+    <div className="border-r border-gray-200 p-5 dark:border-gray-700">
+      <Projects />
     </div>
   )
 }

45121f665a2900902b9278478c97c73a843f915a
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 21:29:34 -0300

    fix(env.ts): update test environment value in NODE_ENV enum to match convention

--- backend/src/infra/config/env.ts
+++ backend/src/infra/config/env.ts
@@ -3,7 +3,7 @@ import z from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum(['production', 'testing', 'development'])
+    .enum(['production', 'test', 'development'])
     .default('development'),
   PORT: z.coerce.number().default(3333),
   JWT_SECRET: z.string(),

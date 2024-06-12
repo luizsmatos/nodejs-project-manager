cf7e0ab7f5ab8760b1a33bb1fb1be9e1a555f5bf
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 22:36:11 -0300

    feat(prisma-users-repository.ts): add new method findById to retrieve a user by id from the database using Prisma ORM

--- backend/src/infra/database/prisma/repositories/prisma-users-repository.ts
+++ backend/src/infra/database/prisma/repositories/prisma-users-repository.ts
@@ -4,6 +4,20 @@ import { PrismaUserMapper } from '../mappers/prisma-user-mapper'
 import { prisma } from '../prisma'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(userId: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+
+    if (!user) {
+      return null
+    }
+
+    return PrismaUserMapper.toDomain(user)
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     const user = await prisma.user.findUnique({
       where: {

7af4285be2d5ac33aa81d0d12101606d58f7d3ae
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-12 16:56:06 -0300

    style(auth.tsx): refactor AuthLayout component layout for better readability and simplicity

--- frontend/src/pages/_layouts/auth.tsx
+++ frontend/src/pages/_layouts/auth.tsx
@@ -1,19 +1,9 @@
-import { ListTodo } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2 antialiased">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
-        <div className="flex items-center gap-3 text-lg text-foreground">
-          <ListTodo className="h-5 w-5" />
-          <span className="font-semibold">project.manager</span>
-        </div>
-        <footer className="text-sm">
-          Project Manager &copy; {new Date().getFullYear()}
-        </footer>
-      </div>
-      <div className="flex flex-col items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center antialiased">
+      <div>
         <Outlet />
       </div>
     </div>

4b65909900bc5aed97024588e28e1e774a8682a2
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-12 14:07:05 -0300

    style(auth.tsx): improve layout structure and styling in AuthLayout component

--- frontend/src/pages/_layouts/auth.tsx
+++ frontend/src/pages/_layouts/auth.tsx
@@ -1,11 +1,19 @@
+import { ListTodo } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
-
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <ListTodo className="h-5 w-5" />
+          <span className="font-semibold">project.manager</span>
+        </div>
+        <footer className="text-sm">
+          Project Manager &copy; {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>

8e0ea5b9794f69feb2ee664b06cfb399ccb8bbdc
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-12 15:46:15 -0300

    feat(account-menu.tsx): create a new AccountMenu component for displaying user account options with dropdown functionality

---
+++ frontend/src/components/account-menu.tsx
@@ -0,0 +1,40 @@
+import { DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu'
+import { ChevronDown, LogOut, UserCog } from 'lucide-react'
+
+import { Button } from './ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
+export function AccountMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="outline"
+          className="flex select-none items-center gap-2"
+        >
+          <UserCog className="h-6 w-6" />
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuLabel className="flex flex-col">
+          <span>Luiz Gustavo</span>
+          <span className="text-xs font-normal text-muted-foreground">
+            luiz.matos@gmail.com
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}

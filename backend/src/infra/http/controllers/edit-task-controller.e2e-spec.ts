3a6ab4eb63b63e829c1918d33015f8974a346fd0
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 23:15:51 -0300

    feat(edit-task-controller): add e2e test for editing a task

---
+++ backend/src/infra/http/controllers/edit-task-controller.e2e-spec.ts
@@ -0,0 +1,47 @@
+import request from 'supertest'
+import { faker } from '@faker-js/faker'
+import { createAndAuthenticateUser } from '#/utils/create-and-authenticate-user'
+import { app } from '../app'
+
+describe('Edit Task Controller (e2e)', () => {
+  it('should return 201 on success', async () => {
+    const { accessToken } = await createAndAuthenticateUser(app)
+
+    const createProject = await request(app)
+      .post('/projects')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: faker.lorem.word(5),
+        description: faker.lorem.paragraph(),
+      })
+
+    const projectId = createProject.body.project.id
+
+    const createTask = await request(app)
+      .post(`/projects/${projectId}/tasks`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: faker.lorem.word(5),
+        description: faker.lorem.paragraph(),
+      })
+
+    const taskId = createTask.body.task.id
+    const updateTitle = faker.lorem.word(5)
+
+    const response = await request(app)
+      .put(`/tasks/${taskId}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        title: updateTitle,
+        description: faker.lorem.paragraph(),
+      })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.task).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        title: updateTitle,
+      }),
+    )
+  })
+})

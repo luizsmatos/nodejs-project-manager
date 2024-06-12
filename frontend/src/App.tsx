76f106e9443613bf49a85627887eda9786276c95
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-12 15:47:30 -0300

    style(App.tsx): reformat App component structure for better readability and maintainability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | project.manager" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="project.manager-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | project.manager" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }

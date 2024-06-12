7634dfbbac42d745b796935d11313cf0b113950d
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-11 21:39:26 -0300

    feat(authenticate-user-controller.ts): add new controller to handle user authentication requests

---
+++ backend/src/infra/http/controllers/authenticate-user-controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express'
+import { z } from 'zod'
+import { makeAuthenticateUserUseCase } from '@/domain/use-cases/factories/make-authenticate-user-usecase'
+
+export async function authenticateUserController(
+  request: Request,
+  response: Response,
+): Promise<Response> {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateBodySchema.parse(request.body)
+
+  const authenticateUserUseCase = makeAuthenticateUserUseCase()
+  const result = await authenticateUserUseCase.execute({
+    email,
+    password,
+  })
+
+  const { accessToken } = result
+
+  return response.status(200).json({ accessToken })
+}

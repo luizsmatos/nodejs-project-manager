fec6852d8bc5bc5c60e0bf47effec844c4a00665
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 08:15:37 -0300

    feat(users-routes.ts): add logout route to clear access_token cookie and send empty response for user logout functionality

--- backend/src/infra/http/routes/users-routes.ts
+++ backend/src/infra/http/routes/users-routes.ts
@@ -7,5 +7,10 @@ const usersRouter = Router()
 
 usersRouter.post('/auth/register', registerUserController)
 usersRouter.post('/auth/login', authenticateUserController)
+usersRouter.post('/auth/logout', (_request, response) => {
+  response.clearCookie('access_token')
+
+  return response.send()
+})
 
 export { usersRouter }

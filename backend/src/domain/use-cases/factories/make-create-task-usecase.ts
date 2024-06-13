6729bb3460c2a54889f38e8c6961969339899c9b
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 20:28:27 -0300

    feat(make-create-task-usecase.ts): add PrismaProjectsRepository dependency to CreateTaskUseCase for improved data handling and retrieval

--- backend/src/domain/use-cases/factories/make-create-task-usecase.ts
+++ backend/src/domain/use-cases/factories/make-create-task-usecase.ts
@@ -1,10 +1,12 @@
+import { PrismaProjectsRepository } from '@/infra/database/prisma/repositories/prisma-projects-repository'
 import { PrismaTasksRepository } from '@/infra/database/prisma/repositories/prisma-tasks-repository'
 import { CreateTaskUseCase } from '../create-task-usecase'
 
 export function makeCreateTaskUseCase(): CreateTaskUseCase {
+  const projectsRepository = new PrismaProjectsRepository()
   const tasksRepository = new PrismaTasksRepository()
 
-  const useCase = new CreateTaskUseCase(tasksRepository)
+  const useCase = new CreateTaskUseCase(projectsRepository, tasksRepository)
 
   return useCase
 }

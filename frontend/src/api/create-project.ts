286ef49f24b34169219d2effa8657802754fe5a6
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 12:35:32 -0300

    feat(api): update createProject function to return project object from response data for better usability

--- frontend/src/api/create-project.ts
+++ frontend/src/api/create-project.ts
@@ -1,13 +1,21 @@
 import { api } from '@/lib/axios'
 
+import { ProjectDTO } from './dtos/project-dto'
+
 interface CreateProjectBody {
   name: string
   description: string
 }
 
+interface CreateProjectResponse {
+  project: ProjectDTO
+}
+
 export async function createProject({ name, description }: CreateProjectBody) {
-  await api.post<CreateProjectBody>('/projects', {
+  const response = await api.post<CreateProjectResponse>('/projects', {
     name,
     description,
   })
+
+  return response.data.project
 }

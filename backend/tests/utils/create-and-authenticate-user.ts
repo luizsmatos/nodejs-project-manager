092902cbebe5e7b0dab5a1f491d88ec9bd5f2136
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 08:45:11 -0300

    feat(create-and-authenticate-user.ts): update return value to include cookies for better handling of authentication in tests

--- backend/tests/utils/create-and-authenticate-user.ts
+++ backend/tests/utils/create-and-authenticate-user.ts
@@ -8,12 +8,12 @@ export async function createAndAuthenticateUser(app: Express) {
     password: '123456',
   })
 
-  const authResponse = await request(app).post('/auth/login').send({
+  const response = await request(app).post('/auth/login').send({
     email: 'nedzesa@sip.cr',
     password: '123456',
   })
 
-  const { accessToken } = authResponse.body
+  const cookies = response.headers['set-cookie']
 
-  return { accessToken }
+  return { cookies }
 }

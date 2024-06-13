d263b11c79edac0976c75cf4901118648e9984d4
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 14:24:09 -0300

    feat(delete-project.tsx): add a new component for deletion projects

---
+++ frontend/src/components/projects/delete-project.tsx
@@ -0,0 +1,73 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
+
+import { deleteProject } from '@/api/delete-project'
+import { ProjectDTO } from '@/api/dtos/project-dto'
+
+import { Button } from '../ui/button'
+import {
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from '../ui/dialog'
+
+interface DeleteProjectProps {
+  project: ProjectDTO
+}
+
+export function DeleteProject({ project }: DeleteProjectProps) {
+  const queryClient = useQueryClient()
+
+  const { mutateAsync: deleteProjectFn } = useMutation({
+    mutationFn: deleteProject,
+    onSuccess(_data, { projectId }) {
+      const cached = queryClient.getQueryData<ProjectDTO[]>(['projects'])
+
+      if (cached) {
+        const filter = cached.filter((item) => item.id !== projectId)
+
+        queryClient.setQueryData<ProjectDTO[]>(['projects'], filter)
+      }
+    },
+  })
+
+  async function handleDeleteProject(projectId: string) {
+    try {
+      await deleteProjectFn({ projectId })
+
+      toast.success('Projeto excluído com sucesso!')
+    } catch (e) {
+      console.log(e)
+      toast.error('Erro ao excluir o projeto!')
+    }
+  }
+
+  return (
+    <>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Deseja realmente excluir?</DialogTitle>
+          <DialogDescription>
+            Essa ação não pode ser desfeita. Todos os dados relacionados a esse
+            projeto serão perdidos.
+          </DialogDescription>
+        </DialogHeader>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="outline">
+              Cancelar
+            </Button>
+          </DialogClose>
+
+          <Button type="button" onClick={() => handleDeleteProject(project.id)}>
+            Excluir
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </>
+  )
+}

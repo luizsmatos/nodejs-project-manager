f310b942178f2231644407d436db16c8e5971195
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 20:29:22 -0300

    feat(create-task-usecase.spec.ts): handle case where project does not exist when creating a new task

--- backend/src/domain/use-cases/create-task-usecase.spec.ts
+++ backend/src/domain/use-cases/create-task-usecase.spec.ts
@@ -1,17 +1,25 @@
 import { faker } from '@faker-js/faker'
 
 import { makeProject } from '#/factories/make-project'
+import { InMemoryProjectsRepository } from '#/repositories/in-memory-projects-repository'
 import { InMemoryTasksRepository } from '#/repositories/in-memory-tasks-repository'
 
+import { TaskStatus } from '../entities/task'
 import { CreateTaskUseCase } from './create-task-usecase'
+import { ProjectNotFoundError } from './errors/project-not-found-error'
 
 let sut: CreateTaskUseCase
+let inMemoryProjectsRepository: InMemoryProjectsRepository
 let inMemoryTasksRepository: InMemoryTasksRepository
 
 describe('Create Task UseCase', () => {
   beforeEach(() => {
+    inMemoryProjectsRepository = new InMemoryProjectsRepository()
     inMemoryTasksRepository = new InMemoryTasksRepository()
-    sut = new CreateTaskUseCase(inMemoryTasksRepository)
+    sut = new CreateTaskUseCase(
+      inMemoryProjectsRepository,
+      inMemoryTasksRepository,
+    )
   })
 
   it('should be able create a new task', async () => {
@@ -21,6 +29,8 @@ describe('Create Task UseCase', () => {
       projectId: project.id,
       title: faker.lorem.word(5),
       description: faker.lorem.paragraph(),
+      status: faker.helpers.enumValue(TaskStatus),
+      userId: faker.string.uuid(),
     })
 
     expect(result.task).toEqual(
@@ -29,5 +39,17 @@ describe('Create Task UseCase', () => {
       }),
     )
     expect(inMemoryTasksRepository.items).toHaveLength(1)
+  })
+
+  it('should not be able create a new task if project does not exist', async () => {
+    await expect(
+      sut.execute({
+        projectId: faker.string.uuid(),
+        title: faker.lorem.word(5),
+        description: faker.lorem.paragraph(),
+        status: faker.helpers.enumValue(TaskStatus),
+        userId: faker.string.uuid(),
+      }),
+    ).rejects.toEqual(new ProjectNotFoundError())
   })
 })

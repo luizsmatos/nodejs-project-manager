c78c36191fa48794628b9b57e6b62622f2ff8084
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 14:55:56 -0300

    feat(account-menu.tsx): add logout functionality

--- frontend/src/components/account/account-menu.tsx
+++ frontend/src/components/account/account-menu.tsx
@@ -1,7 +1,9 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { ChevronDown, LogOut, UserCog } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 import { getUserProfile } from '@/api/get-user-profile'
+import { logout } from '@/api/logout'
 
 import { Button } from '../ui/button'
 import {
@@ -12,12 +14,22 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu'
+import { Skeleton } from '../ui/skeleton'
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const navigate = useNavigate()
+
+  const { data: profile, isLoading: isProfileLoading } = useQuery({
     queryKey: ['user-profile'],
     queryFn: getUserProfile,
     staleTime: Infinity,
+  })
+
+  const { mutateAsync: logoutFn, isPending: isComingOut } = useMutation({
+    mutationFn: logout,
+    onSuccess: () => {
+      navigate('/login', { replace: true })
+    },
   })
 
   return (
@@ -33,13 +45,26 @@ export function AccountMenu() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className="text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
+          {isProfileLoading ? (
+            <div className="space-y-1.5">
+              <Skeleton className="h-4 w-32" />
+              <Skeleton className="h-4 w-20" />
+            </div>
+          ) : (
+            <>
+              <span>{profile?.name}</span>
+              <span className="text-xs font-normal text-muted-foreground">
+                {profile?.email}
+              </span>
+            </>
+          )}
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+        <DropdownMenuItem
+          className="text-rose-500 dark:text-rose-400"
+          disabled={isComingOut}
+          onClick={() => logoutFn()}
+        >
           <LogOut className="mr-2 h-4 w-4" />
           <span>Sair</span>
         </DropdownMenuItem>

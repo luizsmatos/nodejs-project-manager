2bc99d91b8c037e6386ae4189f7ea4b1066c697d
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 16:03:18 -0300

    feat(types): add PaginationResponse interface to define structure for paginated responses in the application

--- backend/src/core/types/pagination-params.ts
+++ backend/src/core/types/pagination-params.ts
@@ -1,3 +1,12 @@
 export interface PaginationParams {
   page: number
 }
+
+export interface PaginationResponse<T> {
+  data: T[]
+  meta: {
+    page: number
+    perPage: number
+    totalCount: number
+  }
+}

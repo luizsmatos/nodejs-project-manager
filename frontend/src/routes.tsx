c82b1cdc44bfc894981986f882d07cb8a9ced902
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 16:37:12 -0300

    refactor(frontend): rename Dashboard component to Home component for better clarity and consistency in naming conventions

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,7 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { NotFound } from './pages/404'
-import { Dashboard } from './pages/app/dashboard'
+import { Home } from './pages/app/home'
 import { Login } from './pages/auth/login'
 import { Register } from './pages/auth/register'
 
@@ -12,7 +12,7 @@ export const router = createBrowserRouter([
     path: '/',
     element: <AppLayout />,
     errorElement: <NotFound />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [{ path: '/home', element: <Home /> }],
   },
   {
     path: '/',

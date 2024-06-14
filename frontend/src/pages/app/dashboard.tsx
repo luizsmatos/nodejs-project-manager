3ab7ebea2c7e68f6cf99c25b34d23dd4bb2bfabb
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 22:34:22 -0300

    feat(dashboard.tsx): add logic to display tasks based on selected project from ProjectContext in Dashboard component

--- frontend/src/pages/app/dashboard.tsx
+++ frontend/src/pages/app/dashboard.tsx
@@ -1,12 +1,26 @@
+import { useContext } from 'react'
 import { Helmet } from 'react-helmet-async'
 
 import { Tasks } from '@/components/tasks/tasks'
+import { ProjectContext } from '@/context/project-context'
 
 export function Dashboard() {
+  const { selectedProject } = useContext(ProjectContext)
+
   return (
     <>
       <Helmet title="Dashboard" />
-      <Tasks />
+      {selectedProject ? (
+        <Tasks project={selectedProject} />
+      ) : (
+        <>
+          <div className="flex h-full items-center justify-center">
+            <p className="text-lg text-muted-foreground">
+              Selecione um projeto para começar
+            </p>
+          </div>
+        </>
+      )}
     </>
   )
 }

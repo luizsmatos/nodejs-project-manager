ebbf8bb63adf6b89a08086516171ea7538f1a944
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 16:08:59 -0300

    feat(list-user-projects-controller): add support for filtering projects by name in the listUserProjectsController function

--- backend/src/infra/http/controllers/list-user-projects-controller.ts
+++ backend/src/infra/http/controllers/list-user-projects-controller.ts
@@ -7,18 +7,20 @@ export async function listUserProjectsController(
   response: Response,
 ): Promise<Response> {
   const listUserProjectsQuerySchema = z.object({
+    name: z.string().optional(),
     page: z.coerce.number().positive().int().optional().default(1),
   })
 
-  const { page } = listUserProjectsQuerySchema.parse(request.query)
+  const { name, page } = listUserProjectsQuerySchema.parse(request.query)
 
   const listUserProjectsUseCase = makeListUserProjectsUseCase()
   const result = await listUserProjectsUseCase.execute({
     userId: request.user.id,
+    name,
     page,
   })
 
-  const { projects } = result
+  const { projects, meta } = result
 
-  return response.status(200).json({ projects })
+  return response.status(200).json({ projects, meta })
 }

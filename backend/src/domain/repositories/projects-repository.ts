41f04353c78c33c1883a4b2dbadc2d54344ce766
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 16:04:13 -0300

    refactor(projects-repository.ts): update findManyByUserId method

--- backend/src/domain/repositories/projects-repository.ts
+++ backend/src/domain/repositories/projects-repository.ts
@@ -1,13 +1,21 @@
-import { PaginationParams } from '@/core/types/pagination-params'
+import {
+  PaginationParams,
+  PaginationResponse,
+} from '@/core/types/pagination-params'
 
 import { Project } from '../entities/project'
+
+export interface FindManyByUserIdParams {
+  userId: string
+  name?: string
+}
 
 export interface ProjectsRepository {
   findById(projectId: string): Promise<Project | null>
   findManyByUserId(
-    userId: string,
+    params: FindManyByUserIdParams,
     pagination: PaginationParams,
-  ): Promise<Project[]>
+  ): Promise<PaginationResponse<Project>>
   delete(project: Project): Promise<void>
   save(project: Project): Promise<void>
   create(project: Project): Promise<void>

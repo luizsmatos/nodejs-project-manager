5a136f7b018be057c7767b97331c5253417d250d
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 17:56:49 -0300

    feat(project-filters.tsx): add support for filtering projects by name using URL query parameters and implement clear filters functionality

--- frontend/src/components/projects/project-filters.tsx
+++ frontend/src/components/projects/project-filters.tsx
@@ -1,40 +1,77 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { SearchIcon } from 'lucide-react'
+import { Search, X } from 'lucide-react'
 import { useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
 
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 
 const projectFiltersSchema = z.object({
-  search: z.string().optional(),
+  name: z.string().optional(),
 })
 
 type ProjectFiltersSchema = z.infer<typeof projectFiltersSchema>
 
 export function ProjectFilters() {
-  const { register, handleSubmit } = useForm<ProjectFiltersSchema>({
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const name = searchParams.get('name')
+
+  const { register, handleSubmit, reset } = useForm<ProjectFiltersSchema>({
     resolver: zodResolver(projectFiltersSchema),
+    defaultValues: {
+      name: name ?? '',
+    },
   })
 
-  function handleProductFilters(data: ProjectFiltersSchema) {
-    console.log(data)
+  function handleFilters({ name }: ProjectFiltersSchema) {
+    setSearchParams((prev) => {
+      if (name) {
+        prev.set('name', name)
+      } else {
+        prev.delete('name')
+      }
+
+      return prev
+    })
+  }
+
+  function handleClearFilters() {
+    setSearchParams((prev) => {
+      prev.delete('name')
+
+      return prev
+    })
+
+    reset({
+      name: '',
+    })
   }
 
   return (
     <form
-      onSubmit={handleSubmit(handleProductFilters)}
+      onSubmit={handleSubmit(handleFilters)}
       className="flex items-center gap-2"
     >
       <Input
         type="text"
         placeholder="Pesquisar"
         className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
-        {...register('search')}
+        {...register('name')}
       />
 
-      <Button type="submit" variant="ghost" size="icon">
-        <SearchIcon className="h-5 w-5" />
+      <Button type="submit" variant="ghost" size="xs">
+        <Search className="h-5 w-5" />
+      </Button>
+
+      <Button
+        onClick={handleClearFilters}
+        type="button"
+        variant="ghost"
+        size="xs"
+      >
+        <X className="h-5 w-5" />
       </Button>
     </form>
   )

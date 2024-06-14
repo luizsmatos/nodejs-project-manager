2ec63170fc33deffeaa5d59337fe694f45d66ee1
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 16:35:38 -0300

    feat(api): enhance listProjectTasks function to support filtering tasks by title, status, and pagination

--- frontend/src/api/list-project-tasks.ts
+++ frontend/src/api/list-project-tasks.ts
@@ -1,24 +1,39 @@
 import { api } from '@/lib/axios'
 
-import { TaskDTO } from './dtos/task-dto'
+import { EnumTaskStatus, TaskDTO } from './dtos/task-dto'
 
 interface ListProjectTasksBody {
   projectId: string
+  title?: string
+  status?: EnumTaskStatus
+  page?: number | null
 }
 
-interface ListProjectTasksResponse {
+export interface ListProjectTasksResponse {
   tasks: TaskDTO[]
+  meta: {
+    page: number
+    perPage: number
+    totalCount: number
+  }
 }
 
-export async function listProjectTasks({ projectId }: ListProjectTasksBody) {
+export async function listProjectTasks({
+  projectId,
+  title,
+  status,
+  page,
+}: ListProjectTasksBody) {
   const response = await api.get<ListProjectTasksResponse>(
     `/projects/${projectId}/tasks`,
     {
       params: {
-        page: 1,
+        title,
+        status,
+        page,
       },
     },
   )
 
-  return response.data.tasks
+  return response.data
 }

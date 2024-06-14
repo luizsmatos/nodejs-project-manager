376867ebe847b5cd42fb3d7d6ec5262fc950ce4a
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 16:36:57 -0300

    fix(login.tsx): update redirect path from '/' to '/home' after successful login to improve user experience

--- frontend/src/pages/auth/login.tsx
+++ frontend/src/pages/auth/login.tsx
@@ -38,7 +38,7 @@ export function Login() {
       await authenticateUserFn({ email: data.email, password: data.password })
       toast.success('Login efetuado com sucesso!')
 
-      navigate('/')
+      navigate('/home')
     } catch (error) {
       toast.error('Credenciais inválidas.')
     }

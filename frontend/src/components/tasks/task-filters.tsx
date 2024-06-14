0ea08941eb32a962450f9a196b039ad04045ab3a
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 17:30:21 -0300

    feat(task-filters.tsx): add support for filtering tasks by title and status using URL search parameters

--- frontend/src/components/tasks/task-filters.tsx
+++ frontend/src/components/tasks/task-filters.tsx
@@ -1,7 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Search, X } from 'lucide-react'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
+import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
+
+import { EnumTaskStatus } from '@/api/dtos/task-dto'
 
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
@@ -14,61 +17,109 @@ import {
 } from '../ui/select'
 
 const taskFiltersSchema = z.object({
-  taskName: z.string().optional(),
-  assignee: z.string().optional(),
+  title: z.string().optional(),
   status: z.string().optional(),
 })
 
 type TaskFiltersSchema = z.infer<typeof taskFiltersSchema>
 
 export function TaskFilters() {
-  const { register, handleSubmit } = useForm<TaskFiltersSchema>({
-    resolver: zodResolver(taskFiltersSchema),
-  })
+  const [searchParams, setSearchParams] = useSearchParams()
 
-  function handleProductFilters(data: TaskFiltersSchema) {
-    console.log(data)
+  const title = searchParams.get('title')
+  const status = searchParams.get('status')
+
+  const { register, handleSubmit, control, reset } = useForm<TaskFiltersSchema>(
+    {
+      resolver: zodResolver(taskFiltersSchema),
+      defaultValues: {
+        title: title ?? '',
+        status: status ?? 'all',
+      },
+    },
+  )
+
+  function handleFilters({ title, status }: TaskFiltersSchema) {
+    setSearchParams((prev) => {
+      if (title) {
+        prev.set('title', title)
+      } else {
+        prev.delete('title')
+      }
+
+      if (status) {
+        prev.set('status', status)
+      } else {
+        prev.delete('status')
+      }
+
+      prev.set('page', '1')
+      return prev
+    })
+  }
+
+  function handleClearFilters() {
+    setSearchParams((prev) => {
+      prev.delete('title')
+      prev.delete('status')
+      prev.set('page', '1')
+      return prev
+    })
+
+    reset({
+      title: '',
+      status: 'all',
+    })
   }
 
   return (
     <form
-      onSubmit={handleSubmit(handleProductFilters)}
+      onSubmit={handleSubmit(handleFilters)}
       className="grid grid-cols-5 items-center gap-2"
     >
       <Input
         type="text"
         placeholder="Nome da tarefa"
         className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
-        {...register('taskName')}
+        {...register('title')}
       />
 
-      <Input
-        type="text"
-        placeholder="Responsável"
-        className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
-        {...register('assignee')}
+      <Controller
+        control={control}
+        name="status"
+        render={({ field }) => (
+          <Select
+            defaultValue="all"
+            name={field.name}
+            onValueChange={field.onChange}
+            value={field.value}
+          >
+            <SelectTrigger className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">Todos status</SelectItem>
+              <SelectItem value={EnumTaskStatus.PENDING}>Pendente</SelectItem>
+              <SelectItem value={EnumTaskStatus.IN_PROGRESS}>
+                Em progresso
+              </SelectItem>
+              <SelectItem value={EnumTaskStatus.DONE}>Feito</SelectItem>
+            </SelectContent>
+          </Select>
+        )}
       />
-
-      <Select defaultValue="all">
-        <SelectTrigger
-          className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
-          {...register('status')}
-        >
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="all">Todos status</SelectItem>
-          <SelectItem value="pending">Pendente</SelectItem>
-          <SelectItem value="completed">Feito</SelectItem>
-        </SelectContent>
-      </Select>
 
       <Button type="submit" variant="secondary" size="xs">
         <Search className="mr-2 h-4 w-4" />
         Filtrar resultados
       </Button>
 
-      <Button variant="ghost" size="xs">
+      <Button
+        onClick={handleClearFilters}
+        type="button"
+        variant="ghost"
+        size="xs"
+      >
         <X className="mr-2 h-4 w-4" />
         Remover filtros
       </Button>

500ce0e029e91d389c1d4755db9a25b74211d58d
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-13 22:30:21 -0300

    feat(edit-task.tsx): refactor EditTask component

--- frontend/src/components/tasks/edit-task.tsx
+++ frontend/src/components/tasks/edit-task.tsx
@@ -1,105 +1,62 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { z } from 'zod'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
-import { Button } from '../ui/button'
+import { TaskDTO } from '@/api/dtos/task-dto'
+import { editTask } from '@/api/edit-task'
+
 import {
-  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '../ui/dialog'
-import { Input } from '../ui/input'
-import { Label } from '../ui/label'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '../ui/select'
-import { Textarea } from '../ui/textarea'
+import { TaskForm, TaskFormSchema } from './task-form'
 
-const editProductSchema = z.object({
-  title: z.string().min(3),
-  description: z.string().min(3).max(191),
-  status: z.string(),
-  completedBy: z.string(),
-})
+interface EditTaskProps {
+  task: TaskDTO
+}
 
-type EditTaskSchema = z.infer<typeof editProductSchema>
+export function EditTask({ task }: EditTaskProps) {
+  const queryClient = useQueryClient()
 
-export function EditTask() {
-  const { register, handleSubmit } = useForm<EditTaskSchema>({
-    resolver: zodResolver(editProductSchema),
+  const { mutateAsync: editTaskFn } = useMutation({
+    mutationFn: editTask,
+    onSuccess(updatedTask) {
+      const cached = queryClient.getQueryData<TaskDTO[]>(['tasks'])
+
+      if (cached) {
+        const updated = cached.map((item) =>
+          item.id === updatedTask.id ? updatedTask : item,
+        )
+
+        queryClient.setQueryData<TaskDTO[]>(['tasks'], updated)
+      }
+    },
   })
 
-  function handleEditTask(data: EditTaskSchema) {
-    console.log(data)
+  async function handleEditTask(data: TaskFormSchema) {
+    try {
+      await editTaskFn({
+        id: task.id,
+        title: data.title,
+        description: data.description,
+        status: data.status,
+      })
+
+      toast.success('Projeto editado com sucesso!')
+    } catch {
+      toast.error('Falha ao editar o projeto, tente novamente!')
+    }
   }
 
   return (
-    <>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Editar tarefa</DialogTitle>
-          <DialogDescription>Edite os detalhes da tarefa</DialogDescription>
-        </DialogHeader>
+    <DialogContent className="sm:max-w-[425px]">
+      <DialogHeader>
+        <DialogTitle>Editar tarefa</DialogTitle>
+        <DialogDescription>Edite os detalhes da tarefa</DialogDescription>
+      </DialogHeader>
 
-        <form onSubmit={handleSubmit(handleEditTask)} className="space-y-6">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="title">Título</Label>
-            <Input className="col-span-3" id="title" {...register('title')} />
-          </div>
-
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="description">Descrição</Label>
-            <Textarea
-              id="description"
-              className="col-span-3 resize-none"
-              {...register('description')}
-            />
-          </div>
-
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="status">Status</Label>
-            <Select>
-              <SelectTrigger
-                id="status"
-                className="col-span-3"
-                {...register('status')}
-              >
-                <SelectValue />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="pending">Pendente</SelectItem>
-                <SelectItem value="completed">Feito</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="completedBy">Pessoa</Label>
-            <Input
-              className="col-span-3"
-              id="completedBy"
-              {...register('completedBy')}
-            />
-          </div>
-
-          <DialogFooter>
-            <DialogClose asChild>
-              <Button type="button" variant="outline">
-                Cancelar
-              </Button>
-            </DialogClose>
-
-            <Button type="submit">Salvar</Button>
-          </DialogFooter>
-        </form>
-      </DialogContent>
-    </>
+      <TaskForm initialValues={task} onSubmit={handleEditTask} />
+    </DialogContent>
   )
 }

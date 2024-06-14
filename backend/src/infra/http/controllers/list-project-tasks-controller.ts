87ebfb5dfb80325c3e35362316ddf152234760ed
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 16:09:15 -0300

    feat(list-project-tasks-controller.ts): add support for filtering tasks by title and status in listProjectTasksController to enhance search functionality

--- backend/src/infra/http/controllers/list-project-tasks-controller.ts
+++ backend/src/infra/http/controllers/list-project-tasks-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { z } from 'zod'
 import { makeListProjectTasksUseCase } from '@/domain/use-cases/factories/make-list-project-tasks-usecase'
+import { TaskStatus } from '@/domain/entities/task'
 
 export async function listProjectTasksController(
   request: Request,
@@ -13,18 +14,24 @@ export async function listProjectTasksController(
   const { projectId } = listProjectTasksParamsSchema.parse(request.params)
 
   const listProjectTasksQuerySchema = z.object({
+    title: z.string().optional(),
+    status: z.nativeEnum(TaskStatus).optional(),
     page: z.coerce.number().positive().int().optional().default(1),
   })
 
-  const { page } = listProjectTasksQuerySchema.parse(request.query)
+  const { title, status, page } = listProjectTasksQuerySchema.parse(
+    request.query,
+  )
 
   const listProjectTasksUseCase = makeListProjectTasksUseCase()
   const result = await listProjectTasksUseCase.execute({
     projectId,
+    title,
+    status,
     page,
   })
 
-  const { tasks } = result
+  const { tasks, meta } = result
 
-  return response.status(200).json({ tasks })
+  return response.status(200).json({ tasks, meta })
 }

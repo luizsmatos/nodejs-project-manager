6e7f651ba051fb32ea4f76c0379570f640466b23
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 22:14:16 -0300

    feat(task-filters.tsx): add filter functionality with a sheet component for project filtering, including search and clear filters options

--- frontend/src/components/tasks/task-filters.tsx
+++ frontend/src/components/tasks/task-filters.tsx
@@ -1,12 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Search, X } from 'lucide-react'
-import { Controller, useForm } from 'react-hook-form'
+import { Filter, Search, X } from 'lucide-react'
+import { useForm } from 'react-hook-form'
 import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
 
 import { EnumTaskStatus } from '@/api/dtos/task-dto'
 
 import { Button } from '../ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form'
 import { Input } from '../ui/input'
 import {
   Select,
@@ -15,6 +16,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from '../ui/select'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
+} from '../ui/sheet'
 
 const taskFiltersSchema = z.object({
   title: z.string().optional(),
@@ -29,15 +37,13 @@ export function TaskFilters() {
   const title = searchParams.get('title')
   const status = searchParams.get('status')
 
-  const { register, handleSubmit, control, reset } = useForm<TaskFiltersSchema>(
-    {
-      resolver: zodResolver(taskFiltersSchema),
-      defaultValues: {
-        title: title ?? '',
-        status: status ?? 'all',
-      },
+  const form = useForm<TaskFiltersSchema>({
+    resolver: zodResolver(taskFiltersSchema),
+    defaultValues: {
+      title: title ?? '',
+      status: status ?? 'all',
     },
-  )
+  })
 
   function handleFilters({ title, status }: TaskFiltersSchema) {
     setSearchParams((prev) => {
@@ -66,63 +72,108 @@ export function TaskFilters() {
       return prev
     })
 
-    reset({
+    form.reset({
       title: '',
       status: 'all',
     })
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(handleFilters)}
-      className="grid grid-cols-5 items-center gap-2"
-    >
-      <Input
-        type="text"
-        placeholder="Nome da tarefa"
-        className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
-        {...register('title')}
-      />
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="xs">
+          <Filter className="h-5 w-5" />
+        </Button>
+      </SheetTrigger>
 
-      <Controller
-        control={control}
-        name="status"
-        render={({ field }) => (
-          <Select
-            defaultValue="all"
-            name={field.name}
-            onValueChange={field.onChange}
-            value={field.value}
-          >
-            <SelectTrigger className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="all">Todos status</SelectItem>
-              <SelectItem value={EnumTaskStatus.PENDING}>Pendente</SelectItem>
-              <SelectItem value={EnumTaskStatus.IN_PROGRESS}>
-                Em progresso
-              </SelectItem>
-              <SelectItem value={EnumTaskStatus.DONE}>Feito</SelectItem>
-            </SelectContent>
-          </Select>
-        )}
-      />
-
-      <Button type="submit" variant="secondary" size="xs">
-        <Search className="mr-2 h-4 w-4" />
-        Filtrar resultados
-      </Button>
-
-      <Button
-        onClick={handleClearFilters}
-        type="button"
-        variant="ghost"
-        size="xs"
+      <SheetContent
+        side="right"
+        className="xs:w-full flex flex-col gap-4 p-4 dark:bg-gray-800 sm:w-80 md:w-80 lg:w-96 lg:gap-6 lg:p-6 xl:w-96"
       >
-        <X className="mr-2 h-4 w-4" />
-        Remover filtros
-      </Button>
-    </form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleFilters)}
+            className="flex flex-col items-center gap-2"
+          >
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Título</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Buscar por título"
+                      className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="status"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel htmlFor="status">Status</FormLabel>
+                  <FormControl>
+                    <Select
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger id="status" className="col-span-3">
+                        <SelectValue />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value={EnumTaskStatus.PENDING}>
+                          Pendente
+                        </SelectItem>
+                        <SelectItem value={EnumTaskStatus.IN_PROGRESS}>
+                          Em progresso
+                        </SelectItem>
+                        <SelectItem value={EnumTaskStatus.DONE}>
+                          Feito
+                        </SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <SheetFooter className="w-full">
+              <div className="mt-6 flex w-full flex-col items-center gap-2">
+                <SheetClose asChild>
+                  <Button
+                    type="submit"
+                    variant="secondary"
+                    size="xs"
+                    className="w-full"
+                  >
+                    <Search className="mr-2 h-4 w-4" />
+                    Filtrar resultados
+                  </Button>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    size="xs"
+                    onClick={handleClearFilters}
+                    className="w-full"
+                  >
+                    <X className="mr-2 h-4 w-4" />
+                    Remover filtros
+                  </Button>
+                </SheetClose>
+              </div>
+            </SheetFooter>
+          </form>
+        </Form>
+      </SheetContent>
+    </Sheet>
   )
 }

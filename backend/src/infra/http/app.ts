1a7031ec7e726f6fdd7d0e9027a3189fa7d923ab
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-15 18:12:12 -0300

    feat(app.ts): add CORS configuration based on environment to restrict allowed origins and improve security

--- backend/src/infra/http/app.ts
+++ backend/src/infra/http/app.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet'
 
 import { errorHandler } from './middlewares/error-handler'
 import { routes } from './routes'
+import { env } from '../config/env'
 
 const app = express()
 
@@ -18,7 +19,24 @@ app.use(
     credentials: true,
     allowedHeaders: ['Content-Type'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    origin: '*',
+    origin: (origin, callback) => {
+      if (!origin) {
+        return callback(null, true)
+      }
+
+      if (env.NODE_ENV === 'development') {
+        return callback(null, true)
+      }
+
+      const allowedOrigins = ['https://project-manager-ui.onrender.com/']
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const message =
+          'The CORS policy for this site does not allow access from the specified Origin.'
+        return callback(new Error(message))
+      }
+
+      return callback(new Error('Not allowed by CORS'))
+    },
   }),
 )
 app.options('*', cors())

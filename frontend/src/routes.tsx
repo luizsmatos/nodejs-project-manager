1f67f2f264e20c56ab439361e9be53bd34be76c5
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-15 14:12:53 -0300

    feat(routes.tsx): add Tasks component to handle displaying tasks for a specific project ID in the app

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { NotFound } from './pages/404'
 import { Home } from './pages/app/home'
+import { Tasks } from './pages/app/tasks'
 import { Login } from './pages/auth/login'
 import { Register } from './pages/auth/register'
 import { Error } from './pages/error'
@@ -13,7 +14,10 @@ export const router = createBrowserRouter([
     path: '/',
     element: <AppLayout />,
     errorElement: <Error />,
-    children: [{ path: '/', element: <Home /> }],
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/boards/:projectId', element: <Tasks /> },
+    ],
   },
   {
     path: '/',

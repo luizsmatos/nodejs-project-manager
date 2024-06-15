c27fef89b9e3c2457fc074def8faaa08bdfdf254
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-15 12:12:40 -0300

    feat(get-user-by-id-controller.ts): add input validation for userId parameter using zod library

--- backend/src/infra/http/controllers/get-user-by-id-controller.ts
+++ backend/src/infra/http/controllers/get-user-by-id-controller.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from 'express'
 import { makeGetUserByIdUseCase } from '@/domain/use-cases/factories/make-get-user-by-id-usecase'
 import { UserPresenter } from './presenters/user-presenter'
+import z from 'zod'
 
 export async function getGetUserByIdController(
   request: Request,
   response: Response,
 ): Promise<Response> {
+  const getUserByIdParams = z.object({
+    userId: z.string().uuid(),
+  })
+
+  const { userId } = getUserByIdParams.parse(request.params)
+
   const getUserByIdUseCase = makeGetUserByIdUseCase()
   const result = await getUserByIdUseCase.execute({
-    userId: request.user.id,
+    userId,
   })
 
   const { user } = result

68e306cc22971f06875fff2b3e4e0417b1daf705
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-14 22:12:47 -0300

    feat(project-filters.tsx): add filter functionality with a sheet component for project filtering, including search and clear filters options

--- frontend/src/components/projects/project-filters.tsx
+++ frontend/src/components/projects/project-filters.tsx
@@ -1,11 +1,19 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Search, X } from 'lucide-react'
+import { Filter, Search, X } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
 
 import { Button } from '../ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel } from '../ui/form'
 import { Input } from '../ui/input'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetTrigger,
+} from '../ui/sheet'
 
 const projectFiltersSchema = z.object({
   name: z.string().optional(),
@@ -15,10 +23,9 @@ type ProjectFiltersSchema = z.infer<typeof projectFiltersSchema>
 
 export function ProjectFilters() {
   const [searchParams, setSearchParams] = useSearchParams()
-
   const name = searchParams.get('name')
 
-  const { register, handleSubmit, reset } = useForm<ProjectFiltersSchema>({
+  const form = useForm<ProjectFiltersSchema>({
     resolver: zodResolver(projectFiltersSchema),
     defaultValues: {
       name: name ?? '',
@@ -44,35 +51,76 @@ export function ProjectFilters() {
       return prev
     })
 
-    reset({
+    form.reset({
       name: '',
     })
   }
 
   return (
-    <form
-      onSubmit={handleSubmit(handleFilters)}
-      className="flex items-center gap-2"
-    >
-      <Input
-        type="text"
-        placeholder="Pesquisar"
-        className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
-        {...register('name')}
-      />
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="xs">
+          <Filter className="h-5 w-5" />
+        </Button>
+      </SheetTrigger>
 
-      <Button type="submit" variant="ghost" size="xs">
-        <Search className="h-5 w-5" />
-      </Button>
-
-      <Button
-        onClick={handleClearFilters}
-        type="button"
-        variant="ghost"
-        size="xs"
+      <SheetContent
+        side="right"
+        className="xs:w-full flex flex-col gap-4 p-4 dark:bg-gray-800 sm:w-80 md:w-80 lg:w-96 lg:gap-6 lg:p-6 xl:w-96"
       >
-        <X className="h-5 w-5" />
-      </Button>
-    </form>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleFilters)}
+            className="flex flex-col items-center gap-2"
+          >
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem className="w-full">
+                  <FormLabel>Nome</FormLabel>
+                  <FormControl className="col-span-3">
+                    <Input
+                      {...field}
+                      placeholder="Digite o nome do projeto"
+                      className="border-none bg-gray-200 text-sm focus:ring-0 dark:bg-gray-700"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <SheetFooter className="w-full">
+              <div className="mt-6 flex w-full flex-col items-center gap-2">
+                <SheetClose asChild>
+                  <Button
+                    type="submit"
+                    variant="secondary"
+                    size="xs"
+                    className="w-full"
+                  >
+                    <Search className="mr-2 h-4 w-4" />
+                    Filtrar resultados
+                  </Button>
+                </SheetClose>
+
+                <SheetClose asChild>
+                  <Button
+                    type="button"
+                    variant="ghost"
+                    size="xs"
+                    onClick={handleClearFilters}
+                    className="w-full"
+                  >
+                    <X className="mr-2 h-4 w-4" />
+                    Remover filtros
+                  </Button>
+                </SheetClose>
+              </div>
+            </SheetFooter>
+          </form>
+        </Form>
+      </SheetContent>
+    </Sheet>
   )
 }

61c80f401403b6060511829a67e3a43ad6ea8d78
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-15 14:12:23 -0300

    chore(frontend): remove sidebar.tsx and project-context.tsx files as they are no longer needed in the project

--- frontend/src/components/sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import { Projects } from './projects/projects'
-
-export function SideBar() {
-  return (
-    <div className="border-r border-gray-200 p-5 dark:border-gray-700">
-      <Projects />
-    </div>
-  )
-}

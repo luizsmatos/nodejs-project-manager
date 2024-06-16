0dab1d6004cfcb4079ed3056a0d17fa1a6043185
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-16 01:26:41 -0300

    style(project-list.tsx): update grid layout classes to be responsive and improve UI/UX on different screen sizes

--- frontend/src/components/projects/project-list.tsx
+++ frontend/src/components/projects/project-list.tsx
@@ -57,7 +57,7 @@ export function ProjectList() {
         <CreateProject />
       </Dialog>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-5">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-6 lg:grid-cols-4 xl:grid-cols-5">
         {isLoadingProjects ? (
           <>
             {Array.from({ length: 20 }).map((_, index) => {

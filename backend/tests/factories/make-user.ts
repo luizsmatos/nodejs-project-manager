a645ec90433f5bff6cebb834c11414071b5771bb
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-17 10:07:18 -0300

    feat(tests): add UserFactory class with makePrismaUser method to create and persist user data using Prisma ORM

--- backend/tests/factories/make-user.ts
+++ backend/tests/factories/make-user.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 
 import { User } from '@/domain/entities/user'
+import { prisma } from '@/infra/database/prisma/prisma'
+import { PrismaUserMapper } from '@/infra/database/prisma/mappers/prisma-user-mapper'
 
 export function makeUser(override: Partial<User> = {}): User {
   return {
@@ -10,5 +12,16 @@ export function makeUser(override: Partial<User> = {}): User {
     password: faker.internet.password(),
     createdAt: faker.date.past(),
     ...override,
+  }
+}
+
+export class UserFactory {
+  static async makePrismaUser(override: Partial<User> = {}): Promise<User> {
+    const user = makeUser(override)
+    await prisma.user.create({
+      data: PrismaUserMapper.toPrisma(user),
+    })
+
+    return user
   }
 }

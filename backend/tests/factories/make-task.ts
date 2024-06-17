86a4d2a3148ae0bc79d96fdd70f899464bc99e2d
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-17 10:07:30 -0300

    feat(tests): add TaskFactory class with makePrismaTask method to create and persist tasks using Prisma ORM

--- backend/tests/factories/make-task.ts
+++ backend/tests/factories/make-task.ts
@@ -1,6 +1,8 @@
 import { faker } from '@faker-js/faker'
 
 import { Task, TaskStatus } from '@/domain/entities/task'
+import { PrismaTaskMapper } from '@/infra/database/prisma/mappers/prisma-task-mapper'
+import { prisma } from '@/infra/database/prisma/prisma'
 
 export function makeTask(override: Partial<Task> = {}): Task {
   return {
@@ -14,5 +16,16 @@ export function makeTask(override: Partial<Task> = {}): Task {
     createdAt: faker.date.past(),
     updatedAt: null,
     ...override,
+  }
+}
+
+export class TaskFactory {
+  static async makePrismaTask(override: Partial<Task> = {}): Promise<Task> {
+    const Task = makeTask(override)
+    await prisma.task.create({
+      data: PrismaTaskMapper.toPrisma(Task),
+    })
+
+    return Task
   }
 }

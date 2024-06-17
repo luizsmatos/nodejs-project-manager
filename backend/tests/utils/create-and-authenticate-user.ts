62355f236589e114601f36aa266bd1fc176857b1
Luiz Gustavo <gusstavopdm@gmail.com>
2024-06-17 10:07:09 -0300

    feat(tests): add logic to fetch user data after authentication for further testing purposes

--- backend/tests/utils/create-and-authenticate-user.ts
+++ backend/tests/utils/create-and-authenticate-user.ts
@@ -1,19 +1,21 @@
 import { Express } from 'express'
 import request from 'supertest'
+import { prisma } from '@/infra/database/prisma/prisma'
 
 export async function createAndAuthenticateUser(app: Express) {
-  await request(app).post('/auth/register').send({
+  await request(app).post('/api/auth/register').send({
     name: 'Edgar Washington',
     email: 'nedzesa@sip.cr',
     password: '123456',
   })
 
-  const response = await request(app).post('/auth/login').send({
+  const response = await request(app).post('/api/auth/login').send({
     email: 'nedzesa@sip.cr',
     password: '123456',
   })
 
   const cookies = response.headers['set-cookie']
+  const user = await prisma.user.findFirst()
 
-  return { cookies }
+  return { cookies, user }
 }
